d712286be913f857ebbef44bffccfd31031e3a07
Anastasia <boshniak.a@gmail.com>
2020-09-12 23:47:47 +0300

    Updates

---
+++ precache-manifest.91cc221a225f15ab0d4b3ad8b79eba6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90e71d5009f358f71b90cf1a11b74919",
+    "url": "/todo-list-react-it-inc/index.html"
+  },
+  {
+    "revision": "773dc0296d78a89d3ea4",
+    "url": "/todo-list-react-it-inc/static/css/main.7efa2b7b.chunk.css"
+  },
+  {
+    "revision": "83455dfd08401193b467",
+    "url": "/todo-list-react-it-inc/static/js/2.1654cff6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo-list-react-it-inc/static/js/2.1654cff6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "773dc0296d78a89d3ea4",
+    "url": "/todo-list-react-it-inc/static/js/main.22bd9aeb.chunk.js"
+  },
+  {
+    "revision": "765d3956b1531ecaf165",
+    "url": "/todo-list-react-it-inc/static/js/runtime-main.e8c6053c.js"
+  }
+]);
