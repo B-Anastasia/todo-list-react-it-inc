e16a58a28469ae4e6b710b33118cbc56df259ef3
Anastasia Boshniak <34798026+B-Anastasia@users.noreply.github.com>
2022-01-12 20:58:43 +0200

    engines

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const port = process.env.PORT || 3000;
 
 app.listen(port, (err)=>{
     if (process.env.NODE_ENV === 'production') {
-        app.use(express.static('build'));
+        app.use(express.static(path.join(__dirname, '../build')));
         app.get('*', (req, res)=>{
-            req.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+            req.sendFile(path.resolve(__dirname, '../build', 'index.html'))
         })
     }
     if (err) return console.log(err);
